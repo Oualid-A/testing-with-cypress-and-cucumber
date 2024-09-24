956202155b20970afe3c1506e9fe1ea711c97c69
Oualid-A <oualid.achrif@ump.ac.ma>
2024-09-24 14:34:59 +0100

    add cucumber report

--- cypress.config.js
+++ cypress.config.js
@@ -1,19 +1,37 @@
-const { defineConfig } = require("cypress");
-const cucumber = require("cypress-cucumber-preprocessor").default;
+const { defineConfig } = require('cypress')
+const cucumber = require('cypress-cucumber-preprocessor').default
+const browserify = require("@cypress/browserify-preprocessor");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-      on("file:preprocessor", cucumber());
-    },
-    specPattern: ['cypress/integration/**/*.feature', 'cypress/tests/**/*.spec.js'],
-    baseUrl: "https://reqres.in/api/",
-    pageLoadTimeout: 10000,
-    screenshotOnRunFailure: true,
-  },
-  video: false,
-  defaultCommandTimeout: 5000,
-  viewportHeight: 900,
-  viewportWidth: 1900,
-});
+	reporter: 'cypress-mochawesome-reporter',
+
+	e2e: {
+		setupNodeEvents(on, config) {
+			const options = browserify.defaultOptions;
+
+			options.browserifyOptions.plugin.unshift(['tsify']);
+			// For the cucumber preprocessor
+			on('file:preprocessor', cucumber())
+			require('cypress-mochawesome-reporter/plugin')(on)
+			return config
+		},
+		specPattern: [
+			'cypress/integration/**/*.feature',
+			'cypress/tests/**/*.spec.js',
+		],
+		baseUrl: 'https://reqres.in/api/',
+		pageLoadTimeout: 10000,
+		screenshotOnRunFailure: true,
+		reporterOptions: {
+			reportDir: 'cypress/reports',
+			charts: true,
+			reportPageTitle: 'My Test Suite',
+			embeddedScreenshots: true,
+			inlineAssets: true,
+		},
+	},
+	video: false,
+	defaultCommandTimeout: 5000,
+	viewportHeight: 900,
+	viewportWidth: 1900,
+})
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -18,32 +18,30 @@ import './commands'
 
 import 'cypress-mochawesome-reporter/register'
 
-Cypress.on('test:after:run', (test, runnable) => {
-    afterEach(() => {
-        const screenshotsFolder = Cypress.config("cypress\screenshots");
-        if (window.cucumberJson?.generate) {
-            const testState = window.testState;
-            const stepResult =
-                testState.runTests[testState.currentScenario.name][testState.currentStep];
-            if (stepResult?.status === "failed") {            
-                const screenshotFileName = `${testState.feature.name} -- ${testState.currentScenario.name} (failed).png`;
-                cy.readFile(
-                    `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
-                    "base64"
-                ).then((imgData) => {
-                    stepResult.attachment = {
-                        data: imgData,
-                        media: { type: "image/png" },
-                        index: testState.currentStep,
-                        testCase: testState.formatTestCase(testState.currentScenario),
-                    };
-                });
-    
-            }
-        }
-    });
-})
+afterEach(() => {
+    setTimeout(() => {}, 3000)
+    const screenshotsFolder = Cypress.config("cypress\screenshots\**\*");
+    if (window.cucumberJson?.generate) {
+          const testState = window.testState;
+          const stepResult =
+              testState.runTests[testState.currentScenario.name][testState.currentStep];
+          if (stepResult?.status === "failed") {
+              const screenshotFileName = `${testState.currentScenario.name} -- after each hook (failed).png`;
+              cy.readFile(
+                  `${screenshotsFolder}/${Cypress.spec.name}/${screenshotFileName}`,
+                  "base64"
+              ).then((imgData) => {
+                  stepResult.attachment = {
+                      data: imgData,
+                      media: { type: "image/png" },
+                      index: testState.currentStep,
+                      testCase: testState.formatTestCase(testState.currentScenario),
+                  };
+              });
 
+          }
+      }
+});
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from failing the test
     return false;
